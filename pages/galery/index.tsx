a693fadc7607d62720653b4a8955da7c84a320f1
muhammad bilal <harli.ramli@erasysconsulting.com>
2023-04-17 18:33:43 +0700

    update

--- pages/galery/index.tsx
+++ pages/galery/index.tsx
@@ -3,24 +3,29 @@ import { HeroComp } from '../../components/Hero';
 import CardGalery from '../../customComp/cardGalery';
 
 export default function GaleryPage({ data, dataHero }: any) {
-  const rawDtKurban = data[0].attributes.img_kurban.data;
-  const kurban = rawDtKurban.map((item: { attributes: any }) => item.attributes);
-  const rawDtSantunan = data[0].attributes.img_santunan.data;
-  const santunan = rawDtSantunan.map((item: { attributes: any }) => item.attributes);
+  // console.log(data)
+  const map = data.map((item, idx) => item)
+  const map1 = map.map(item => item.attributes)
+  console.log(map1)
+  // const rawDtKurban = data[0].attributes.img_kurban.data;
+  // const kurban = rawDtKurban.map((item: { attributes: any }) => item.attributes);
+  // const rawDtSantunan = data[0].attributes.img_santunan.data;
+  // const santunan = rawDtSantunan.map((item: { attributes: any }) => item.attributes);
 
   return (
     <>
-      <div style={{ marginTop: -200 }}>
+      {/* <div style={{ marginTop: -200 }}>
         <HeroComp data={dataHero} seeCardDonasi={false} withButton={false} dataMargin={rem(200)} />
       </div>
       <CardGalery data={kurban} title="Kurban" />
-      <CardGalery data={santunan} title="Santunan" />
+      <CardGalery data={santunan} title="Santunan" /> */}
+      tes
     </>
   );
 }
 
 export async function getServerSideProps() {
-  const res = await fetch(`https://strapi.yathim.or.id/api/galeries?populate=*`);
+  const res = await fetch(`http://localhost:1337/api/galeries?populate=*`);
   const data = await res.json();
   const fetchHero = await fetch(
     `http://strapi.yathim.or.id/api/home-pages?filters[id][$eq]=3&populate=*`
